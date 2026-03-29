3e6a34ca1d0466c81b5d61eb3af902d60beddf85
kaixin168sxz <122666772+kaixin168sxz@users.noreply.github.com>
2026-03-29 12:47:53 +0800

    refactor(models): merge MessageData and MessageDataArgs

--- src/pyrite_sdk/models/schema.py
+++ src/pyrite_sdk/models/schema.py
@@ -2,16 +2,16 @@ from pydantic import BaseModel, Field
 from .consts import *
 from typing import Optional
 
-class MessageDataArgs(BaseModel):
-    manager: str = Field(..., description="The manager name")
-
 class MessageData(BaseModel):
-    args: Optional[MessageDataArgs] = Field(None, description="The arguments")
     err: Optional[str] = Field(None, description="The error")
+    manager: Optional[str] = Field(None, description="The manager RFW code")
 
-class BasicMessage(BaseModel):
+class Message(BaseModel):
     cmd: MessageCommands = Field(..., description="The command")
     data: MessageData = Field(..., description="The data")
-    source: Optional["BasicMessage"] = Field(None, description="The source message")
+    source: Optional["Message"] = Field(None, description="The source message")
 
-BasicMessage.model_rebuild()
+Message.model_rebuild()
+
+# x = Message.model_validate_json('{"cmd":"GetRfwCode","data":{"args":"manager0"}}')
+# print(x)
