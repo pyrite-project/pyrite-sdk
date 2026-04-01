896fa8371f63f92617b6327b93d6a5d52610e2aa
microcrab-project <youremail@example.com>
2026-04-01 17:01:05 +0800

    feat: added match-case and used [] to access var

--- src/pyrite_sdk/api/ui/base.py
+++ src/pyrite_sdk/api/ui/base.py
@@ -4,16 +4,20 @@ from .event import Event
 class Var(RFWSerializable):
     def __init__(self, *paths):
         self.paths = paths
+    
+    def __getitem__(self, key):
+        return Var(*self.paths, key)
+
     def to_rfw(self):
         return ".".join(map(str, self.paths))
 
-def data(*paths): return Var("data", *paths)
-def args(*paths): return Var("args", *paths)
-def state(*paths): return Var("state", *paths)
+data = Var("data")
+args = Var("args")
+state = Var("state")
 
-def data_(*paths): return f"$[{data(*paths).to_rfw()}]"
-def args_(*paths): return f"$[{args(*paths).to_rfw()}]"
-def state_(*paths): return f"$[{state(*paths).to_rfw()}]"
+def data_(*paths): return f"$[{Var('data', *paths).to_rfw()}]"
+def args_(*paths): return f"$[{Var('args', *paths).to_rfw()}]"
+def state_(*paths): return f"$[{Var('state', *paths).to_rfw()}]"
 
 class Widget(RFWSerializable):
     def __init__(self, name: str, **kwargs):
@@ -49,3 +53,19 @@ class Widget(RFWSerializable):
     def to_rfw(self):
         args_str = ", ".join(f"{key}: {_serialize_value(value)}" for (key, value) in self.args.items() if key != None and value != None)
         return f"{self.name}({args_str})"
+
+class Match(RFWSerializable):
+    def __init__(self, var, *cases):
+        self.cases = cases
+        self.var = var
+    
+    def to_rfw(self):
+        return f"switch {self.var} {{ {', '.join([a_case.to_rfw() for a_case in self.cases])} }}"
+
+class Case(RFWSerializable):
+    def __init__(self, cond, value):
+        self.cond = cond
+        self.value = value
+    
+    def to_rfw(self):
+        return f"{_serialize_value(self.cond)}: {_serialize_value(self.value)}"
