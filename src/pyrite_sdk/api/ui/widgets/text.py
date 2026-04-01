582a6688e9680496acf4f0cc6c36b53ddf96ebaf
microcrab-project <youremail@example.com>
2026-04-01 16:34:28 +0800

    fix: use the new way to parse string

--- src/pyrite_sdk/api/ui/widgets/text.py
+++ src/pyrite_sdk/api/ui/widgets/text.py
@@ -1,10 +1,10 @@
 from ..base import Widget
-from ....utils.ui import parse
+from ....utils.ui import DataParser
 
 class Text(Widget):
     def __init__(self, text:str | list, style = None, text_direction = None, **kwargs):
         super().__init__("Text",
-                        text = parse(text) if isinstance(text, str) else text,
+                        text = DataParser(text).to_rfw() if isinstance(text, str) else text,
                         style = style,
                         textDirection = text_direction,
                         **kwargs)
