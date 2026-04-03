0be3c762370f44e64e27d7931f4a94d75b8d5f23
kaixin168sxz <122666772+kaixin168sxz@users.noreply.github.com>
2026-04-03 11:08:04 +0800

    refactor: renamed the arg widget to child

--- src/pyrite_sdk/api/ui/widgets/state.py
+++ src/pyrite_sdk/api/ui/widgets/state.py
@@ -1,14 +1,14 @@
 from ..base import Widget
 
 class State(Widget):
-    def __init__(self, widget: Widget, **kwargs: dict[str:str]):
+    def __init__(self, child: Widget, **kwargs: dict[str:str]):
         super().__init__("State")
-        self.widget = widget
+        self.child = child
         self.states = kwargs
-        self.to_rfw = self.widget.to_rfw
+        self.to_rfw = self.child.to_rfw
 
     def setup(self):
         super().setup()
-        self.widget.parent = self
-        self.widget.manager = self.manager
-        self.widget.setup()
+        self.child.parent = self
+        self.child.manager = self.manager
+        self.child.setup()
