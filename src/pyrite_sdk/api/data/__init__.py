f37ae406d3abc6101f9ecadbeaab549a19c881c9
Can1425 <myqf1425@126.com>
2026-07-22 23:19:43 +0800

    feat!: add i18n support

--- src/pyrite_sdk/api/data/__init__.py
+++ src/pyrite_sdk/api/data/__init__.py
@@ -1,5 +1,6 @@
 from .theme import Theme
 from .i18n import I18n
+from .i18n_keys import I18nKey, fallback_messages
 from .stubs import Stubs
 
-__all__ = ["Theme", "I18n", "Stubs"]
+__all__ = ["Theme", "I18n", "I18nKey", "fallback_messages", "Stubs"]
